50adf0113f734dd22252adda3396d2fe53f0738c
Peter Makafan <makafanpeter@gmail.com>
2016-02-25 19:58:15 +0100

    updated Config

--- config.py
+++ config.py
@@ -8,12 +8,12 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     OAUTH_CREDENTIALS =  {
     'facebook': {
-          'id': '470154729788964',
-          'secret': '010cc08bd4f51e34f3f3e684fbdea8a7'
+          'id': '939475312801278',
+          'secret': '33a13a651e8a0ccbbfd795b2a719bc92'
       },
       'google': {
-          'id': '763910754530-rqig5i0qt95f2832bdkg15m49580rhtl.apps.googleusercontent.com',
-          'secret': 'KA5kZILEXedg8SuQsied7MfW'
+          'id': '434855738061-2tdjkrh80gnm717rr5658u6lp8pmo4db.apps.googleusercontent.com',
+          'secret': '6EjBpvzF59V9kp_Pi7BdW9QC'
       },
       'twitter': {
           'id': '3RzWQclolxWZIMq5LJqzRZPTl',
